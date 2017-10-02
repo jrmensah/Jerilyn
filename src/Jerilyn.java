2110b3352f10daca460f8716a4de04bbe4b10677
jrmensah <jmensah4@student.umuc.edu>
2017-10-02 14:52:41 -0400

    First commit- App for myself

---
+++ src/Jerilyn.java
@@ -0,0 +1,33 @@
+public class Jerilyn {
+    public static void main(String args[]){
+
+        //Declare all variables
+
+        int firstName,lastName,myAge;
+
+        //All three
+        String = firstName, lastName, myAge;
+
+        // Questions ask my name and my age
+        String question = "What is your name?";
+        String question2= "What is your age?";
+
+        //Assign values to variables
+
+        // String question = "What is your name?";
+        firstName = "Jerilyn";
+        lastName = "Mensah";
+
+        // String question2 = "What is your age?";
+        myAge = "30";
+
+
+        //    Print my first name
+        System.out.println("My name is" + firstName + lastName );
+    /*Print my age */
+        System.out.println("I am" + myAge + "years old.");
+
+
+    }
+
+}
